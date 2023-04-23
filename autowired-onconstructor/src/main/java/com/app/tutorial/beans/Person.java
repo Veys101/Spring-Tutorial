20efd30efd5f7523c8e26f0f07d29b5857bfdf59
Veys101 <vys2626@gmail.com>
2023-04-24 01:13:04 +0300

    autowired on constructor

---
+++ autowired-onconstructor/src/main/java/com/app/tutorial/beans/Person.java
@@ -0,0 +1,30 @@
+package com.app.tutorial.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    private String name;
+
+    // To prevent changes, "final" keyword added to the vehicle object
+    private final Vehicle vehicle;
+
+    @Autowired
+    public Person(Vehicle vehicle) {
+        System.out.println("Person bean created by Spring");
+        this.vehicle = vehicle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+}
