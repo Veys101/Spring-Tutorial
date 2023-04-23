92f6c58ac85639e91cc2b92f92098447b15da113
Veys101 <vys2626@gmail.com>
2023-04-24 00:55:48 +0300

    autowired on setter method

---
+++ autowired-onsettermethod/src/main/java/com/app/tutorial/config/ProjectConfig.java
@@ -0,0 +1,10 @@
+package com.app.tutorial.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.app.tutorial")
+public class ProjectConfig {
+    
+}
---
+++ autowired-onsettermethod/src/main/java/com/app/tutorial/main/App.java
@@ -0,0 +1,24 @@
+package com.app.tutorial.main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.app.tutorial.beans.Person;
+import com.app.tutorial.beans.Vehicle;
+import com.app.tutorial.config.ProjectConfig;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        vehicle.setName("Honda");
+        Person person = context.getBean(Person.class);
+        person.setName("Alice");
+        System.out.println("Person name from Spring Context is: " + person.getName());
+        System.out.println("Vehicle name from Spring Context is: " + vehicle.getName());
+        System.out.println("Vehicle that Person own is: " + person.getVehicle());
+        context.close();
+
+    }
+}
