f8bd31e69fe0db7ded26b483f935692d0ac389d1
Veys101 <vys2626@gmail.com>
2023-05-03 00:38:16 +0300

    spring boot mvc added

---
+++ springboot-mvc/src/main/java/com/app/tutorial/SpringbootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.app.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+}
---
+++ springboot-mvc/src/main/java/com/app/tutorial/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.app.tutorial.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping(value = {"", "/", "/home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Harry Jane");
+        return "home.html";
+    }
+}
