8c383b77da33e552ed2aabc67a209b655c153514
Veys101 <vys2626@gmail.com>
2023-05-04 01:45:53 +0300

    inner directory gitignore removed

---
+++ springboot-school-mvc/src/main/java/com/app/tutorial/springbootschoolmvc/SpringbootSchoolMvcApplication.java
@@ -0,0 +1,13 @@
+package com.app.tutorial.springbootschoolmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSchoolMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSchoolMvcApplication.class, args);
+	}
+
+}
