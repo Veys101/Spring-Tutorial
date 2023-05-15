9a262d6b990d9e978021a88178d1e02b2c92de2d
Veys101 <vys2626@gmail.com>
2023-05-16 01:09:06 +0300

    InMemoryUserDetailsManager added

---
+++ springboot-school-web-security/src/main/java/com/app/tutorial/springbootschoolwebsecurity/config/ProjectSecurityConfig.java
@@ -0,0 +1,78 @@
+package com.app.tutorial.springbootschoolwebsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    /*
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       http.authorizeHttpRequests().anyRequest().permitAll()
+               .and().formLogin()
+               .and().httpBasic();
+
+        return http.build();
+    }*/
+
+    /*@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().denyAll()
+                .and().formLogin()
+                .and().httpBasic();
+
+        return http.build();
+    }*/
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests()
+                .requestMatchers("/home", "", "/").authenticated()
+                .requestMatchers("/holidays/**").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/saveMsg").permitAll()
+                .requestMatchers("/courses").authenticated()
+                .requestMatchers("/about").permitAll()
+                .requestMatchers("/assets/**").permitAll()
+                .and().formLogin().and().httpBasic();
+
+        return http.build();
+
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("123456"))
+                .roles("USER")
+                .build();
+
+        System.out.println(user.getPassword());
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("654321"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        System.out.println(admin.getPassword());
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
