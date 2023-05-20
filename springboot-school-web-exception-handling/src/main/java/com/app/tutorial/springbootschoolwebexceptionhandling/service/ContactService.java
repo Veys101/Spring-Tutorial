028fef17b9ad668846359e8198e541cb59fcf6ae
Veys101 <vys2626@gmail.com>
2023-05-20 23:13:33 +0300

    update exception handling project

---
+++ springboot-school-web-exception-handling/src/main/java/com/app/tutorial/springbootschoolwebexceptionhandling/service/ContactService.java
@@ -0,0 +1,32 @@
+package com.app.tutorial.springbootschoolwebexceptionhandling.service;
+
+import com.app.tutorial.springbootschoolwebexceptionhandling.model.Contact;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Slf4j
+@Service
+//@RequestScope
+@SessionScope
+//@ApplicationScope
+public class ContactService {
+
+    private int counter = 0;
+
+    public ContactService() {
+        System.out.println("Contact service initialized.");
+    }
+
+    public void saveContact(Contact contact) {
+        log.info(contact.toString());
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+}
